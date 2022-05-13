e3f6c5cb4ca805038118bb364bee2421226eebcb
Qiyao Chen <1113938836@qq.com>
2022-05-13 21:00:31 +0800

    add err

--- Astar_numpy.py
+++ Astar_numpy.py
@@ -7,7 +7,7 @@ class Astar:
         print('Imput Grid:\n',self.grid)
         self.found = False  # 到达标记
         self.resign = False  # 启动标记
-        self.cost=1
+        self.cost=1#每移动一步花费的cost
         self.begin_point=np.array([0,0],dtype=np.int32)
         self.target_point=np.array([0,0],dtype=np.int32)
         self.W=len(grid[0])#grid宽度
@@ -23,10 +23,10 @@ class Astar:
     def set_start(self,startx,starty):#输入起始点X，Y坐标
         self.begin_point[0]=startx
         self.begin_point[1]=starty
-    def set_target(self,targetx,targety):
+    def set_target(self,targetx,targety):#设置终点函数
         self.target_point[0]=targetx
         self.target_point[1]=targety
-    def x_y_inrange(self,xin,yin):
+    def x_y_inrange(self,xin,yin):#检查x、yin是否超出地图边界
         if xin>=0 and xin<self.H and yin>=0 and yin<self.W:
             return True
         else:
@@ -57,11 +57,12 @@ class Astar:
 
         found = False  # 到达标记
         resign = False  # 启动标记
-        steps=0
+        steps=1
         while not found and not resign:
             if len(cell)==0:
                 resign=True
-                return
+                print('寻路失败~\n请检查路径是否封闭')
+                exit()
             else:
                 cell=cell[np.argsort(cell[:,0])]
                 next_point=cell[0]
@@ -73,9 +74,11 @@ class Astar:
                 g=next_point[1]
                 x=next_point[2]
                 y=next_point[3]
-
+    
                 if x==target_point[0] and y==target_point[1]:
                     found=True
+                    self.realcost=cell[0][0]
+                    print(cell)
                 else:
                     for i in range(len(delta)):#计算周围四个坐标参数
                         x_new=x+delta[i][0]#x坐标跟新
@@ -86,12 +89,13 @@ class Astar:
                                 g_new=g+self.cost
                                 f_new=g_new+heuristic[x_new][y_new]
                                 c_new=np.array([[f_new,g_new,x_new,y_new]],dtype=np.int32)
-                                print('xnew=',x_new,'ynew=',y_new)
+                                print('STEPS=',steps,'xnew=',x_new,'ynew=',y_new)
                                 cell=np.r_[cell,c_new]#numpy数组中加入新的点位信息
                                 #print('new cell\n',cell)
                                 close_matrix[x_new][y_new]=1
                                 action_matrix[x_new][y_new]=i
-                steps+=1
+                                steps+=1
+                
         path=np.array([[target_point[0],target_point[1]]],dtype=np.int32)
         x=target_point[0]
         y=target_point[1]
@@ -103,13 +107,13 @@ class Astar:
             y_pre=y-delta[action_matrix[x][y]][1]
             x=x_pre
             y=y_pre
-            grid_copy[x][y]=5#黄色
+            grid_copy[x][y]=10#路径颜色
             path=np.r_[[[x,y]],path]
         self.path=path
-        grid_copy[grid_copy==1]=10#障碍物颜色
+        grid_copy[grid_copy==1]=5#障碍物颜色
         self.path_grid=grid_copy
-        print('path is :\n',path)
-        print(grid_copy)
+        #print('path is :\n',path)
+        #print(grid_copy)
     def show_mat(self,title):
         plt.matshow(self.path_grid)
         plt.title(title)
@@ -119,25 +123,26 @@ class Astar:
 
 
 if __name__ == "__main__":
-    
+    #grid为地图矩阵，其中1为障碍物，0表示空旷
     grid = [
        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
        [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1],
        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-       [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
+       [0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+       [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+       [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0]]
     
-    '''[[0, 1, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0], 
-            [0, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0]]'''
+    #生成算法实例对象
     k=Astar(grid)
+    #设置路径起点
     k.set_start(9,0)
+    #设置目标终点
     k.set_target(9,28)
+    #计算路径
     k.calculate()
-    k.show_mat('Path Output,STEPS='+str(k.steps))
+    #矩阵绘制显示
+    k.show_mat('Path Output,STEPS='+str(k.steps)+'    COST='+str(k.realcost))
