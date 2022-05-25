ca65b9480516ce3ba0300810f504e897e00ff65f
Qiyao Chen <1113938836@qq.com>
2022-05-25 20:47:21 +0800

    modify

--- Astar_list.py
+++ Astar_list.py
@@ -1,15 +1,16 @@
 #A*算法是一个最优路径查找的算法
 #可以根据不同动作的损失来找到达到目标最近的方法
 #本算法目前就考虑了路径的上下左右移动，尚未涉及斜对角移动
+#使用list替换numpy，比较速度影响
 #版本说明 V1.1
-#日期   2022/5/13
+#日期   2022/5/20
 #作者   Qiyao
 #https://gitee.com/C-Qiyao/astar-algorithm-py
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-
+#图像二值化
 def read_img(filename):
     im=Image.open(filename)
     Img = im.convert('L')
@@ -22,17 +23,17 @@ def read_img(filename):
             table.append(0)
     photo = Img.point(table, '1')
     imag=np.array(photo,dtype=np.int0)
-    return imag
+    return imag.tolist()
 
 class Astar:
     def __init__(self,grid):
-        self.grid=np.array(grid,dtype=np.int0)
-        print('Imput Grid:\n',self.grid)
+        self.grid=grid
+        print('Imput Grid:\n',len(self.grid),len(self.grid[0]))
         self.found = False  # 到达标记
         self.resign = False  # 启动标记
         self.cost=1#每移动一步花费的cost
-        self.begin_point=np.array([0,0],dtype=np.int32)
-        self.target_point=np.array([0,0],dtype=np.int32)
+        self.begin_point=[0,0]
+        self.target_point=[0,0]
         self.W=len(grid[0])#grid宽度
         self.H=len(grid)#grid高度
         self.shape=(self.H,self.W)
@@ -40,9 +41,9 @@ class Astar:
                      [0, -1],  # 左
                      [1, 0],  # 下
                      [0, 1]]  # 右
-        self.heuristic = np.zeros(self.shape,dtype=np.int32)#生成与grid同大小的0矩阵
-        self.close_matrix= np.zeros(self.shape,dtype=np.int32)#生成与grid同大小的0矩阵,用来保存周围坐标
-        self.action_matrix= np.zeros(self.shape,dtype=np.int32)#生成与grid同大小的0矩阵，用来保存已经计算过的点
+        self.heuristic = [[0 for row in range(self.W)] for col in range(self.H)]#生成与grid同大小的0矩阵
+        self.close_matrix= [[0 for row in range(self.W)] for col in range(self.H)]#生成与grid同大小的0矩阵,用来保存周围坐标
+        self.action_matrix= [[0 for row in range(self.W)] for col in range(self.H)]#生成与grid同大小的0矩阵，用来保存已经计算过的点
 
     def set_start(self,startx,starty):#输入起始点X，Y坐标
         self.begin_point[0]=startx
@@ -61,22 +62,25 @@ class Astar:
     def calculate(self):
         assert(self.grid[self.begin_point[0]][self.begin_point[1]]!=1)and(self.grid[self.target_point[0]][self.target_point[1]]!=1)
         #检查起点和终点是不是在障碍物区内
-        self.start=time.time()
+        
         target_point=self.target_point
         delta=self.delta
-        grid_copy=self.grid.copy()#生成grid的副本用于添加路径信息以及可视化内容
+        grid_copy=np.array(self.grid.copy(),dtype=np.int32)#生成grid的副本用于添加路径信息以及可视化内容
+        #print(grid_copy)
         #计算H-cost
+        self.start=time.time()
         for i in range(self.H):#逐个计算与终点的距离值
             for j in range(self.W):
                 self.heuristic[i][j] = abs(i - target_point[0]) + abs(j - target_point[1])
                 if grid[i][j] == 1:#如果遇到障碍，默认给一个巨大的值，让他不可取
                     self.heuristic[i][j] = 999 
-        print('H-coat Grid:\n',self.heuristic)
+        #print('H-coat Grid:\n',self.heuristic)
         x = self.begin_point[0]
         y = self.begin_point[1]
         g = 0
         f = g + self.heuristic[self.begin_point[0]][self.begin_point[1]]
-        cell = np.array([[f, g, x, y]],dtype=np.int32)
+        cell = [[f, g, x, y]]
+        #print(cell)
         #print(cell)
 
         found = False  # 到达标记
@@ -88,9 +92,10 @@ class Astar:
                 print('寻路失败~\n请检查路径是否封闭')
                 exit()
             else:
-                cell=cell[np.argsort(cell[:,0])]
-                next_point=cell[0]
-                cell=cell[1:]
+                cell.sort()  # 从大到小排序
+                cell.reverse()#反转排序方向，从小到大
+                next_point = cell.pop()#取出当前的最小值
+                #print(next_point)
                 #print(cell)
                 #print(next_point)
                 ######$$$$$$$
@@ -112,18 +117,17 @@ class Astar:
                             if self.close_matrix[x_new][y_new]==0 and self.grid[x_new][y_new]==0:
                                 g_new=g+self.cost
                                 f_new=g_new+self.heuristic[x_new][y_new]
-                                c_new=np.array([[f_new,g_new,x_new,y_new]],dtype=np.int32)
+                                c_new=[f_new,g_new,x_new,y_new]
                                 #print('STEPS=',steps,'xnew=',x_new,'ynew=',y_new)
-                                cell=np.r_[cell,c_new]#numpy数组中加入新的点位信息
+                                cell.append(c_new)#数组中加入新的点位信息
                                 #print('new cell\n',cell)
                                 self.close_matrix[x_new][y_new]=1
                                 self.action_matrix[x_new][y_new]=i
                                 steps+=1
                 
-        path=np.array([[target_point[0],target_point[1]]],dtype=np.int32)
+        path=[[target_point[0],target_point[1]]]
         x=target_point[0]
         y=target_point[1]
-        
         self.steps=steps
         #开始根据action回溯路径
         while x!=self.begin_point[0] or y!=self.begin_point[1]:
@@ -132,9 +136,11 @@ class Astar:
             x=x_pre
             y=y_pre
             grid_copy[x][y]=10#路径颜色
-            path=np.r_[[[x,y]],path]
+            path.append([x,y])
         self.end=time.time()
-        self.path=path
+        path_zheng=[]#此处pasth为倒叙
+        for i in range(len(path)):
+            path_zheng.append(path[len(path) - 1 - i])
         grid_copy[grid_copy==1]=5#障碍物颜色
         self.path_grid=grid_copy
 
@@ -159,7 +165,7 @@ if __name__ == "__main__":
        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
        [0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0]]'''
-    while(1):
+    if 1:#方便改成while
         grid= read_img('mig2.png')
     #生成算法实例对象
         k=Astar(grid)
